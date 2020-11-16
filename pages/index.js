d2ad9d8a77413168d12521251f3a02cde3ffa8e8
Ehsan Umar <shafaque@zaavya.com>
2020-11-16 20:44:49 +0500

    Add an empty state The Polaris Empty state component helps to communicate the value of your app and its primary action when merchants first add it to their Shopify admin.

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,15 @@ import { Layout, Page, EmptyState } from '@shopify/polaris';
 const Index = () => (
   <Page>
     <Layout>
-      <EmptyState variation="positive">
-        Sample app using React and Next.js
+    <EmptyState
+        heading="Discount your products temporarily"
+        action={{
+          content: 'Select products',
+          onAction: () => console.log('clicked'),
+        }}
+        image={img}
+      >
+        <p>Select products to change their price temporarily.</p>
       </EmptyState>
     </Layout>
   </Page>
